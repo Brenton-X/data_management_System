7e01ec1d38c3d9b5b81be096d7e0ab4ec3e766f0
Brenton <brentonxie@gmail.com>
2024-08-20 21:18:06 +1000

    updata

--- page/login/index.js
+++ page/login/index.js
@@ -5,3 +5,32 @@
  * 1.3 基于 axios 调用验证码登录接口
  * 1.4 使用 Bootstrap 的 Alert 警告框反馈结果给用户
  */
+
+// 1.2
+document.querySelector('.btn').addEventListener('click', () => {
+  const form = document.querySelector('.login-form')
+  const data = serialize(form, { hash: true, empty: true })
+
+  // 1.3
+  axios({
+    url: '/v1_0/authorizations',
+    method: 'POST',
+    data
+  }).then(result => {
+    // 1.4
+    myAlert(true, '登录成功')
+    console.log(result)
+
+    // 登陆成功保存令牌
+    localStorage.setItem('token', result.data.token)
+    setTimeout(() => {
+      location.href = '../content/index.html'
+    }, 1500
+    )
+
+  }).catch(error => {
+    console.log(error)
+    myAlert(false, error.response.data.message)
+    console.dir(error.response.data.message)
+  })
+})
